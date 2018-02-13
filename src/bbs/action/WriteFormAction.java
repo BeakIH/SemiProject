ed0b6f52cec9a6f162c8eeae07d7dc9a29eec90d
user1 <user1@DESKTOP-BIBKLL9>
2018-02-13 18:38:27 +0900

    커밋

---
+++ src/bbs/action/WriteFormAction.java
@@ -0,0 +1,25 @@
+package bbs.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WriteFormAction implements CommandAction {//글 입력 폼 처리
+
+    public String requestPro(HttpServletRequest request,
+        HttpServletResponse response)throws Throwable {
+
+        int bNo = 0;
+        try{ 
+        	
+          if(request.getParameter("bNo")!=null){
+        	  bNo=Integer.parseInt(request.getParameter("bNo"));
+
+	   }
+	}catch(Exception e){e.printStackTrace();}
+		//해당 뷰에서 사용할 속성
+		request.setAttribute("bNo", new Integer(bNo));
+
+
+		return "/listo/notice/writeForm.jsp";//해당 뷰
+	}
+}
