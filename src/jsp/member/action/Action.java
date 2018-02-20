c8e70085252a1ac74f02cec59eba9b855fa83a18
user1 <user1@DESKTOP-BIBKLL9>
2018-02-20 12:18:53 +0900

    윤지> 회원가입 1차

--- src/jsp/member/action/Action.java
+++ src/jsp/member/action/Action.java
@@ -1,9 +1,9 @@
-package action;
-
+package jsp.member.action;
+ 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public interface CommandAction {
+public interface Action {
    public String requestPro(HttpServletRequest request,HttpServletResponse response) throws Throwable;
    
 }
